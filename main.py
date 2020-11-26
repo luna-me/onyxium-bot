6a0e86fc9b107f8c9779538693591bb488e25e59
noor <40831855+noorPy@users.noreply.github.com>
2020-11-26 11:18:43 -0500

    added help, reaction to message

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import discord
 
-token = "1234"
+from discord.ext import commands
+
+token = ""
 prefix = "/"
 
-client = discord.Client()
+client = commands.Bot(command_prefix = prefix) 
+client.remove_command('help')
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(name='onyxium'))
+
+@client.command(pass_context=True)
+async def help(ctx):
+	author = ctx.message.author
+	embed=discord.Embed(title="onyxium", description="help commands", color=0x7CFC00)
+	embed.add_field(name="ping", value="Bot latency", inline=True)
+
+	await author.send(embed=embed)
+
+	last_message = ctx.channel.last_message_id
+	message = await ctx.channel.fetch_message(int(last_message))
+	await message.add_reaction(emoji='✅')
 
 client.run(token)
 
